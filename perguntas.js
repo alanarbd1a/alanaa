d863529b6101f2e3a690afabdecac18e21c60b0c
alanarbd1a <146251278+alanarbd1a@users.noreply.github.com>
2024-11-18 15:36:31 +0000

    181124

--- perguntas.js
+++ perguntas.js
@@ -1,50 +1,33 @@
 criaCartao(
     '1-',
-    'Benefícios que o esporte nós proporciona?',
-    'Além de melhorar a aptidão física, o exercício físico regular também pode melhorar a capacidade cognitiva e reduzir os níveis de ansiedade e 
-    estresse em geral.  Os exercícios ajudam a melhorar a autoestima, a imagem corporal, 
-    a cognição e a função social de pacientes em risco de saúde mental.'
-    
+    'Importância de uma alimentação saudável?',
+    'ajuda a proteger contra a má nutrição em todas as suas formas.'
 )
 
 criaCartao(
     '2-',
-    'Quais os esportes que mais estão em alta na nova geração?',
-    'Os esportes mais populares no geral são respectivamente: futebol, voleibol e basquetebol.
-     Ambos tem dois times para cada lado e o objetivo final e marcar mais pontos.'
+    'Importância da prática de esportes?',
+    'melhora nosso ânimo e capacidade cognitiva, proporciona mais disposição, libera hormônios.'
 )
 
 criaCartao(
     '3-',
-    'A importância de uma alimentação saudável para o desenvolvimento físico?',
-    'A alimentação está entre um dos principais pilares da saúde. Os alimentos são a maior e melhor fonte de nutrientes essenciais para o bom 
-    funcionamento do corpo e, consequentemente, contribuem para uma maior qualidade de vida e bem-estar. Por esse motivo é tão importante ter 
-    uma alimentação adequada e saudável.'
+    'Por que devmos regular o sono?',
+    'Pessoas que dormem bem durante a noite andam mais contentes e bem-dispostas durante o dia.'
 )
 
 criaCartao(
     '4-',
-    'Como preservar a saúde mental ?'
-    'Busque o equilíbrio. Tente manter uma organização na realização das tarefas de aula e trabalho, equilibrando as responsabilidades com 
-    as atividades de lazer e descanso. 
- Cuide do corpo.
- Mantenha boas relações. 
- Procure ajuda.
-Saiba onde procurar ajuda.'
+    'Cuidados para saúde mental?',
+    'Cuidar do corpo, buscar equílibrio e manter boas relações.'
 )
 criaCartao(
     '5-',
-    'Exercicíos para extimular a saúde mental',
-    'Meditação.
-Relaxamento muscular progressivo.
-Respiração.
-Massagem.
-Yoga.
-Alongamento.'
+    'Cuidados com o corpo?',
+    'Atividades físicas, sono regular de qualidade e cuidados com a alimentação.'
 )
 criaCartao(
     '6-',
-    'Alimentação saúdavel?',
-    'Uma alimentação saudável é aquela que garante o fornecimento de todos os nutrientes necessários para o funcionamento do nosso corpo. 
-    Investir em alimentos pouco processados e reduzir o consumo de gordurras, sal e açúcar são algumas das medidas que podem melhorar a sua alimentação.'
+    'Dicas de melhoramento físico?',
+    'Beber bastante líquido, práticar atividades físicase uma alimentação balanceada.'
 )
