5d2ee7bab84627ca111b12879172e067611ac78e
alanarbd1a <146251278+alanarbd1a@users.noreply.github.com>
2024-11-18 14:43:10 +0000

    181124

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,50 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    '1-',
+    'Benefícios que o esporte nós proporciona?',
+    'Além de melhorar a aptidão física, o exercício físico regular também pode melhorar a capacidade cognitiva e reduzir os níveis de ansiedade e 
+    estresse em geral.  Os exercícios ajudam a melhorar a autoestima, a imagem corporal, 
+    a cognição e a função social de pacientes em risco de saúde mental.'
+    
 )
 
 criaCartao(
-    'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação.'
+    '2-',
+    'Quais os esportes que mais estão em alta na nova geração?',
+    'Os esportes mais populares no geral são respectivamente: futebol, voleibol e basquetebol.
+     Ambos tem dois times para cada lado e o objetivo final e marcar mais pontos.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é CSS?',
-    'O CSS é uma lingugem de estilização!'
+    '3-',
+    'A importância de uma alimentação saudável para o desenvolvimento físico?',
+    'A alimentação está entre um dos principais pilares da saúde. Os alimentos são a maior e melhor fonte de nutrientes essenciais para o bom 
+    funcionamento do corpo e, consequentemente, contribuem para uma maior qualidade de vida e bem-estar. Por esse motivo é tão importante ter 
+    uma alimentação adequada e saudável.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+    '4-',
+    'Como preservar a saúde mental ?'
+    'Busque o equilíbrio. Tente manter uma organização na realização das tarefas de aula e trabalho, equilibrando as responsabilidades com 
+    as atividades de lazer e descanso. 
+ Cuide do corpo.
+ Mantenha boas relações. 
+ Procure ajuda.
+Saiba onde procurar ajuda.'
 )
 criaCartao(
-    'Programação',
-    'O que é VSCode?',
-    'O Visual Studio Code é um editor de código com diversas integrações.s'
+    '5-',
+    'Exercicíos para extimular a saúde mental',
+    'Meditação.
+Relaxamento muscular progressivo.
+Respiração.
+Massagem.
+Yoga.
+Alongamento.'
 )
 criaCartao(
-    'Programação',
-    'O que é GitHub?',
-    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+    '6-',
+    'Alimentação saúdavel?',
+    'Uma alimentação saudável é aquela que garante o fornecimento de todos os nutrientes necessários para o funcionamento do nosso corpo. 
+    Investir em alimentos pouco processados e reduzir o consumo de gordurras, sal e açúcar são algumas das medidas que podem melhorar a sua alimentação.'
 )
